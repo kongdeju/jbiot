ef12eb9c279e0e66fa0e510b5792c8c13e20c675
KongDeju <kongdeju@gene.ac>
2018-02-26 14:27:32 +0800

    added

--- setup.py
+++ setup.py
@@ -9,19 +9,28 @@
 """
 
 import sys
+import os
 from setuptools import setup
 
 def readfile(filename):
     with open(filename) as f:
         return f.read()
 
+def find_bins():
+    bins = []
+    for root,dirs,files in os.walk("./bin"):
+        for py in files:
+            py = os.path.join(root,py)
+            bins.append(py)
+    return bins
+            
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
           use_pyscaffold=True,
         install_requires=readfile("requirements.txt"),
-        scripts=["bin/xls2tsv.py","bin/tsv2xls.py","bin/md2html.py","bin/lsub.py"]
+        scripts=find_bins()
         )
 
 
