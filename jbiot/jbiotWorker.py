206c58faf13dd42a47b79a57da22853822d5585a
KongDeju <kongdeju@gene.ac>
2018-03-05 09:03:13 +0800

    jbiotWorker support for no run.cmd call

--- jbiot/jbiotWorker.py
+++ jbiot/jbiotWorker.py
@@ -1,12 +1,13 @@
 from JYTools.JYWorker import RedisWorker
-
+import os
 
 class jbiotWorker(RedisWorker):
 
     def execMyfunc(self,myfunc,params):
         output = myfunc(params)
-        cmds = ["sh","run.cmd"]
-        self.execute_subprocess(cmds)
+        if os.path.exists("run.cmd"):
+            cmds = ["sh","run.cmd"]
+            self.execute_subprocess(cmds)
         for k,v in output.items():
             self.set_output(k,v)
 
