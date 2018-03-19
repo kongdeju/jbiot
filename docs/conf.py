c2ce975262ae2388296cca30350f2b6c4f7f772f
KongDeju <kongdeju@gene.ac>
2018-03-19 11:04:12 +0800

    docs/exclude jbio

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,8 @@ release = ''  # Is set by calling `setup.py docs`
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+jbiodir = os.path.abspath("../jbiot/jbio")
+exclude_patterns = ['_build',jbiodir]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
