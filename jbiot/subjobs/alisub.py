da43667ebd22caf91a653b6b61b12be868a5bbf7
KongDeju <kongdeju@gene.ac>
2018-03-31 18:30:30 +0800

    alisub

---
+++ jbiot/subjobs/alisub.py
@@ -0,0 +1,20 @@
+
+
+
+
+def alisub(cmdfile):
+    """sub cmdfile to aliyun batch-compute.
+    mainly to handle docker input and output through analysize cmdfile...
+
+    Args: 
+        cmdfile (str) : the input cmdfile in cmdfile format
+
+    Returns:
+        nothing...
+    """
+    pass
+
+
+
+
+
